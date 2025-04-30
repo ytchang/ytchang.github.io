ae948ee9a2f8f11502fec2bc2e1e9d3a7db471b8
ytchang <j22641305@gmail.com>
2025-04-29 17:19:33 -0700

    Additional Works

--- src/pages/Gallery.jsx
+++ src/pages/Gallery.jsx
@@ -131,7 +131,7 @@ function Gallery() {
           className={`${GalleryCSS.moreWorksToggle} ${isMoreWorksExpanded ? GalleryCSS.expanded : ''}`}
           onClick={() => setIsMoreWorksExpanded(!isMoreWorksExpanded)}
         >
-          More Works
+          Additional Works
           <svg width="12" height="12" viewBox="0 0 12 12" fill="none" xmlns="http://www.w3.org/2000/svg">
             <path d="M2 4L6 8L10 4" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
           </svg>
